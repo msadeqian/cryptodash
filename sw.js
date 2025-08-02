88fdd1f04fd0a3845ce9c04cc9e98e860c7e7718
msadeqian <msadeqian@gmail.com>
2025-08-02 15:01:13 +0330

    icon , change path

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('cryptodash-v1').then((cache) => {
       return cache.addAll([
-        '/cryptodash/index.html',
-        '/cryptodash/icon-144.png',
-        '/cryptodash/icon-180.png',
-        '/cryptodash/icon-192.png',
-        '/cryptodash/icon-512.png'
+        'index.html',
+        'icon-144.png',
+        'icon-180.png',
+        'icon-192.png',
+        'icon-512.png'
         // Add other assets (CSS, JS, etc.) here if needed
       ]);
     })
